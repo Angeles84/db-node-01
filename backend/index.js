32dc65e798096813645a84a0164a36e622ddad4a
Angeles <angie.oliva@gmail.com>
2025-08-02 18:00:51 -0400

    segundo desafío

--- backend/index.js
+++ backend/index.js
@@ -30,8 +30,8 @@ app.get("/posts", async (req, res) => {
 // POST /posts
 app.post("/posts", async (req, res) => {
   const { titulo, img, descripcion, likes } = req.body;
-  if (!titulo) {
-    return res.status(400).json({ message: "Title is required" });
+  if (!titulo || !img || !descripcion) {
+    return res.status(400).json({ message: "Los campos título, imagen y descripción son obligatorios" });
   }
   try {
     const newPosts = await model.createPosts(titulo, img, descripcion, likes);
@@ -41,6 +41,46 @@ app.post("/posts", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+
+app.put("/posts/like/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("ID recibido para like:", id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ message: "ID inválido" });
+  }
+  try {
+    const result = await model.darLike(id);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Post no encontrado" });
+    }
+    return res.status(200).json({
+      message: "Le has dado like al post",
+      post: result.rows[0],
+    });
+  } catch (error) {
+    console.error("Error al incrementar likes:", error);
+    return res.status(500).json({ message: "Error interno en el servidor" });
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await model.eliminarPost(id)
+    if (result.rowCount === 0) {
+      return res.status(404).json({message: "Post no encontrado"});
+    }
+    res.status(200).json({message: 'Post eliminado correctamente'});
+    return res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error al eliminar post:", error);
+    return res.status(500).json({ message: "Error interno en el servidor" });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
--- backend/models/model.js
+++ backend/models/model.js
@@ -12,7 +12,23 @@ const createPosts = async (titulo, img, descripcion, likes) => {
   return results.rows
 };
 
+const eliminarPost = async(id) => {
+  const consulta = "DELETE FROM posts WHERE id = $1 RETURNING *";
+  const values = [id];
+  const result = await pool.query(consulta, values);
+  return result.rows[0];
+};
+
+const darLike = async (id) => {
+  const consulta = "UPDATE posts SET likes = COALESCE(likes, 0) + 1 WHERE id = $1 RETURNING *";
+  const values = [id];
+  const result = await pool.query(consulta, values);
+  return result; // retorna el objeto completo con .rows
+};
+
 export const model = {
   getPosts,
   createPosts,
+  eliminarPost, 
+  darLike
 };
